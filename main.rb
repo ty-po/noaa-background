288fd6b2b6e6426a03e00931939d96ee7bcd11b5
Tyler Polen <tyler.polen@gmail.com>
2019-09-13 18:26:51 -0400

    bundler init, download of most recent image

--- main.rb
+++ main.rb
@@ -1,4 +1,17 @@
 #!/usr/bin/ruby
+require "bundler/setup"
+require "down"
+require "rmagick"
 
-puts "qwef"
+
+time = Time.now
+
+url = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/FD/GEOCOLOR/"\
+      "#{time.year}#{time.yday}#{time.hour}#{time.min.floor(-1)}"\
+      "_GOES16-ABI-FD-GEOCOLOR-5424x5424.jpg"
+
+puts url
+
+Down.download(url, destination: "./")
+
 
