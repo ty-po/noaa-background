80f7c9123b26a401baacafc6af77feeb42c48d5a
Tyler Polen <tyler.polen@gmail.com>
2019-09-14 00:45:48 -0400

    working

--- main.rb
+++ main.rb
@@ -2,16 +2,43 @@
 require "bundler/setup"
 require "down"
 require "rmagick"
+include Magick
+
+def downloadRawImage(time)
+  # theres a bug here probably cause of UTC? idk dont need this anymore unless
+  # i want to animate
+  url = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/FD/GEOCOLOR/"\
+        "#{time.year}#{time.yday}#{time.hour}#{time.min.floor(-1)}"\
+        "_GOES16-ABI-FD-GEOCOLOR-5424x5424.jpg"
+
+  puts url
+
+  Down.download(url, destination: "./current.jpg")
+
+end
 
 
-time = Time.now
+url = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/FD/GEOCOLOR/5424x5424.jpg"
 
-url = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/FD/GEOCOLOR/"\
-      "#{time.year}#{time.yday}#{time.hour}#{time.min.floor(-1)}"\
-      "_GOES16-ABI-FD-GEOCOLOR-5424x5424.jpg"
+imageX = 5424
+imageY = 5424
 
-puts url
+leftX = 2560
+leftY = 1440
 
-Down.download(url, destination: "./")
+rightX = 1920
+rightY = 1080
 
+Down.download(url, destination: "./current.jpg")
 
+raw = ImageList.new("current.jpg")
+
+left=raw.crop(NorthWestGravity,imageX/2, imageY/2)
+left.resize_to_fill!(leftX, leftY, NorthGravity)
+left.display
+left.write("left.jpg")
+
+right=raw.crop(NorthEastGravity,imageX/2, imageY/2)
+right.resize_to_fill!(leftX, leftY, NorthGravity)
+right.display
+right.write("right.jpg")
